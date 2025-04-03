ff67a65a770f6aba8655693abe65a566211dc7bd
Mustapha Benchahyd <m.benchahyd0679@uca.ac.ma>
2025-04-03 21:07:34 +0000

    Re-added backend as normal folder, not submodule

---
+++ backend/controllers/userController.js
@@ -0,0 +1,41 @@
+const jwt = require('jsonwebtoken');
+// User model
+const User = require('../models/userModel');
+
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET_KEY, {expiresIn: '1h'});
+}
+
+// Login User
+const loginUser = async (req, res) => {
+
+    const {email, password} = req.body;
+
+    try {
+        const user = await User.login(email, password);
+
+        const token = createToken(user._id);
+
+        res.status(200).json({email, token});
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
+}
+
+// Signup User
+const signupUser = async (req, res) => {
+
+    const {username, email, password} = req.body;
+
+    try {
+        const user = await User.signup(username, email, password);
+
+        const token = createToken(user._id);
+
+        res.status(200).json({username, email, token});
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
+}
+
+module.exports = {loginUser, signupUser};
---
+++ backend/server.js
@@ -0,0 +1,35 @@
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+const PORT = process.env.PORT || 4000;
+
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/user');
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+});
+
+// Routes
+app.use('/api/user', userRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log('connected to db & listening on port', process.env.PORT)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  }
+)
